a74ab321eac6c0ec239804606c173cda356cd17b
Susan Andrews <97679329+Susan-Andrews@users.noreply.github.com>
2022-09-07 09:38:05 +0530

    Add files via upload

---
+++ email_slicer.py
@@ -0,0 +1,21 @@
+# collect email from user
+# split the email using the  @ ,save the first part as user name and second part as domain name
+# split domain using the .
+
+import email
+
+
+def main():
+    print("WELCOME TO EMAIL SLICER")
+    print("") # blank space
+
+    emailinput=input("Enter your email: ")
+
+    (username,domain)=emailinput.split("@")
+    (domain,extension)=domain.split(".")
+
+    print("Username :",username)
+    print("Domain :",domain)
+    print("Extension :",extension)
+while True:
+    main()
